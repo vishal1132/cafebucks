b91d2fad879b39ca5357c9a42e0c035913acd5e5
vishal1132 <vishals@iitbhilai.ac.in>
2020-09-10 17:44:57 +0530

    commiting offsets

--- eventbus/eventbus.go
+++ eventbus/eventbus.go
@@ -2,6 +2,8 @@ package eventbus
 
 import (
 	"context"
+	"fmt"
+	"log"
 
 	"github.com/rs/zerolog"
 	"github.com/segmentio/kafka-go"
@@ -73,6 +75,12 @@ type Config struct {
 
 	// KafkaClient is the *kafka.AdminClient
 	KafkaClient *kafka.Client
+
+	// GroupID is the consumer group
+	GroupID string
+
+	// Brokers is kafka brokers
+	Brokers []string
 }
 
 // EB is the eventbus struct, which satisfies E
@@ -80,7 +88,51 @@ type EB struct {
 	w *kafka.Writer
 	r *kafka.Reader
 
-	l *zerolog.Logger
+	l   *zerolog.Logger
+	g   *kafka.ConsumerGroup
+	gen *kafka.Generation
+	c   *kafka.Client
+}
+
+// GetOffset returns the offset for the reader
+func (eb *EB) GetOffset(ctx context.Context, topic, group string) (int64, error) {
+	m, err := eb.c.ConsumerOffsets(ctx, kafka.TopicAndGroup{
+		Topic:   topic,
+		GroupId: group,
+	})
+	if err != nil {
+		return -5, err
+	}
+	return m[0], nil
+}
+
+// SetOffset sets the offset for the reader
+func (eb *EB) SetOffset(offset int64) error {
+	return eb.r.SetOffset(offset)
+}
+
+// GetOffsetGen returns the offset for the group
+func (eb *EB) GetOffsetGen(ctx context.Context, topic string) (int64, error) {
+
+	assignments := eb.gen.Assignments[topic]
+	var offset int64
+	for _, assignment := range assignments {
+		_, offset = assignment.ID, assignment.Offset
+	}
+
+	return offset, nil
+
+	// return eb.gen.CommitOffsets(map[string]map[int]int64{topic: {0: offset}})
+}
+
+// SetOffsetGen sets the offset for the group and returns error if any
+func (eb *EB) SetOffsetGen(ctx context.Context, topic string, offset int64) error {
+	var err error
+	eb.gen, err = eb.g.Next(ctx)
+	if err != nil {
+		return err
+	}
+	return eb.gen.CommitOffsets(map[string]map[int]int64{topic: {0: offset}})
 }
 
 // compile time check: does *EB satisfy E interface?
@@ -90,21 +142,52 @@ var _ E = (*EB)(nil)
 func New(cfg Config) (*EB, error) {
 
 	w := kafka.NewWriter(kafka.WriterConfig{
-		Brokers: []string{"localhost:9092"},
+		Brokers: cfg.Brokers,
 		Topic:   cfg.Topic,
 	})
 
 	r := kafka.NewReader(kafka.ReaderConfig{
-		Brokers: []string{"localhost:9092"},
+		Brokers: cfg.Brokers,
 		Topic:   cfg.Topic,
+		GroupID: cfg.GroupID,
 	})
+
+	// group, err := kafka.NewConsumerGroup(kafka.ConsumerGroupConfig{
+	// 	ID:      cfg.GroupID,
+	// 	Brokers: cfg.Brokers,
+	// 	Topics:  []string{"my-topic"},
+	// })
+
+	// if err != nil {
+	// 	return nil, err
+	// }
+
+	c := kafka.NewClient(cfg.Brokers...)
 
 	eb := &EB{
 		w: w,
 		r: r,
 		l: cfg.Logger,
+		c: c,
 	}
+
 	return eb, nil
+}
+
+// NewReader returns a reader
+func (eb *EB) NewReader(brokers []string, topic string) *EB {
+	r := kafka.NewReader(kafka.ReaderConfig{
+		Brokers: brokers,
+		Topic:   topic,
+	})
+	return &EB{r: r}
+}
+
+// NewGen creates a new generation from kafka group
+func (eb *EB) NewGen(ctx context.Context) error {
+	var err error
+	eb.gen, err = eb.g.Next(ctx)
+	return err
 }
 
 // Publish publishes to kafka topic
@@ -124,4 +207,38 @@ func (eb *EB) ReadEvents(ctx context.Context) (kafka.Message, error) {
 		return kafka.Message{}, err
 	}
 	return msg, nil
+}
+
+// StartGen starts a new gen
+func (eb *EB) StartGen() {
+
+}
+
+// ReadEventsGen read events for a consumer group
+func (eb *EB) ReadEventsGen(ctx context.Context, topic string) {
+	var offset int64
+	eb.gen.Start(func(ctx context.Context) {
+		// create reader for this partition.
+		reader := eb.r
+		defer reader.Close()
+		reader.SetOffset(offset)
+		for {
+			msg, err := reader.ReadMessage(ctx)
+			switch err {
+			case kafka.ErrGenerationEnded:
+				// generation has ended.  commit offsets.  in a real app,
+				// offsets would be committed periodically.
+				log.Println("committing offset now")
+				eb.gen.CommitOffsets(map[string]map[int]int64{"test": {0: offset + 1}})
+				return
+			case nil:
+				offset = msg.Offset
+				return
+			default:
+				fmt.Printf("error reading message: %+v\n", err)
+				return
+			}
+		}
+	})
+
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module github.com/vishal1132/cafebucks
 go 1.14
 
 require (
+	github.com/klauspost/compress v1.11.0 // indirect
+	github.com/pierrec/lz4 v2.5.2+incompatible // indirect
 	github.com/rs/zerolog v1.19.0
 	github.com/segmentio/kafka-go v0.4.2
 	github.com/valyala/fastjson v1.5.4
